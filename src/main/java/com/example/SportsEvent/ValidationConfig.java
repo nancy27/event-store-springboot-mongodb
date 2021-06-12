9f8e80b45be0817f1e2b14c1e160ad9dec2d5ea1
nancy27 <54560171+nancy27@users.noreply.github.com>
2021-06-11 22:46:16 -0500

    added gradle dependency

--- src/main/java/com/example/SportsEvent/ValidationConfig.java
+++ src/main/java/com/example/SportsEvent/ValidationConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+
 @Configuration
 public class ValidationConfig {
     @Bean
